7478cb0b6a866cd523fd9f5cc04b3a45a73bef6e
leo812leo <leo_812leo@hotmail.com.tw>
2021-07-11 23:31:25 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,21 +3,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const restaurant = require('./models/restaurantSchema')
+//mongoose
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error.')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected.')
+})
+
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 // express template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 定義要使用的樣板引擎
 app.set('view engine', 'handlebars') //設定的 view engine 是 handlebars
-
+// body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
 // setting static files
 app.use(express.static('public'))
 
+
 // routes setting
+// index
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+
+// search
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
   const restaurants_filter = restaurantList.results.filter(restaurant => {
@@ -30,7 +49,9 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurantList.results[Number(req.params.restaurant_id) - 1] })
 })
 
-//start and listen the Express sereve
+
+
+//listener
 app.listen(port, () => {
   console.log(`Express is listening on localhost ${port}`)
 })
