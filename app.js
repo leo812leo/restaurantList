0b9a86f2e7fcb18f0bbb0010f0d17d46adb4f468
leo812leo <leo_812leo@hotmail.com.tw>
2021-07-12 03:34:24 +0800

    feat: add delete resturants function

--- app.js
+++ app.js
@@ -92,8 +92,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 
 })
-
-
+// delete
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 
 //detial
