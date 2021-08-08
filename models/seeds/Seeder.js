6abfffa293ecaa179918fc67e146fee14bb9e899
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-09 04:01:08 +0800

    modify Seeder

---
+++ models/seeds/Seeder.js
@@ -0,0 +1,40 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const bcrypt = require('bcryptjs')
+// DB_data
+const Restaurant = require('../restaurant')
+const User = require('../user')
+// seed
+const SEED_USER = require('./userSeed.json')
+const SEED_RESTAURANT = require('./restaurant.json')
+// connect to db
+const db = require('../../config/mongoose')
+
+// 連線成功
+db.once('open', () => {
+  Promise.all(
+    SEED_USER.map(user =>
+      bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(user.password, salt))
+        .then(hash =>
+          User.create({
+            name: user.name,
+            email: user.email,
+            password: hash
+          }))
+        .then(userDB => {
+          const restaurants = SEED_RESTAURANT.filter(restaurant => user.belongRestaurants.includes(restaurant.id))
+          console.log('user', user)
+          restaurants.forEach(restaurant => { restaurant.userId = userDB._id })
+          console.log('--------------')
+          console.log('restaurants', restaurants)
+          return Restaurant.create(restaurants)
+        })
+    ))
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+    .catch(error => console.log(error))
+})
