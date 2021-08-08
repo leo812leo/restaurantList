3b87e57597b25115ef7345ae5235e8b69dbc81ba
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-08 10:24:02 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 /* import User define modules */
 const routes = require('./routes')
 require('./config/mongoose')
@@ -19,15 +20,20 @@ app.set('view engine', 'handlebars') //設定的 view engine 是 handlebars
 app.use(express.urlencoded({ extended: true }))
 // setting static files
 app.use(express.static('public'))
-//methodOverride
+// methodOverride
 app.use(methodOverride('_method'))
+// session
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 // 將 request 導入路由器
 app.use(routes)
 
-
 // 設定Server Port
 const port = 3000
-//listener
+// listener
 app.listen(port, () => {
   console.log(`Express is listening on localhost ${port}`)
 })
